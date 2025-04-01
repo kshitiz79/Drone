8e135ff111f254b656fd485032337a5e7247f7cd
ManupriyaNagar <manupriyanagar2@gmail.com>
2025-04-01 16:58:37 +0530

    banner changed

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -72,14 +72,14 @@ const Hero = () => {
   };
 
   return (
-    <div className="relative bg-gradient-to-br from-[#103c65] to-[#328dcc] h-[100vh]  md:h-auto">
+    <div className="relative bg-gradient-to-br from-[#103c65] to-[#328dcc] h-[100vh]  md:h-[125vh]">
       <Slider {...settings} className="h-full">
         {sliderImages.map((image, index) => (
           <div key={index} className="h-full">
             <img
               src={image}
               alt={`Slide ${index + 1}`}
-              className="w-full lg:h-[86vh] h-[100vh] object-cover"
+              className="w-full lg:h-[125vh] h-[100vh] md:h-[125vh] object-cover"
             />
           </div>
         ))}
@@ -93,9 +93,9 @@ const Hero = () => {
           <DroneBackgroundSVG className="absolute bottom-10 right-10 opacity-20" />
         </div>
 
-        <div className="max-w-7xl mx-auto px-6 md:px-14 w-full flex flex-col lg:flex-row justify-between relative z-10 md:mt-20 lg:mt-0">
+        <div className="max-w-7xl mx-auto px-6 md:px-14 w-full flex flex-col lg:flex-row justify-between relative z-10 md:mt-80 lg:mt-44">
           {/* Left Side - Text and CTA (Position Unchanged) */}
-          <div className="w-full lg:w-1/2 text-white">
+          <div className="w-full lg:w-1/2 text-white mt-20">
             <h1 className="lg:text-6xl md:text-4xl text-2xl mb-4 font-sans font-thin bg-gradient-to-r from-black to-blue-600 bg-clip-text text-transparent animate-fade-in">
               Drone Planet: <br /> Shaping the Future of Flight
             </h1>
@@ -111,11 +111,11 @@ const Hero = () => {
           <div className="w-full lg:w-2/5 mt-8 md:mt-10 lg:mt-0">
 
 
-
+<div className="mt-30 h-30 w-30">
           
       <form
         onSubmit={handleSubmit}
-        className="bg-white  backdrop-blur-md px-6 py-3 rounded-xl shadow-lg   lg:space-y-3 max-w-md mx-auto lg:mx-0 border border-gray-700"
+        className="bg-white backdrop-blur-md px-6 py-10 rounded-xl shadow-lg lg:space-y-3 max-w-50px lg:mx-0 border border-gray-700"
       >
         <div className="bg-white p-4 rounded-lg space-y-3">
           <h3 className="text-lg font-bold text-center uppercase text-[#027bff] animate-fade-in">
@@ -289,6 +289,7 @@ const Hero = () => {
           )}
         </div>
       </form>
+      </div>
 
 
 
